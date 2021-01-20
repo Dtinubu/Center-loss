948e2dc76c8f1b294cf98846676a554a429bf255
Dtinubu <73240396+Dtinubu@users.noreply.github.com>
2021-01-20 11:28:28 -0500

    Update trainer.py

--- trainer.py
+++ trainer.py
@@ -158,5 +158,5 @@ class Trainer(object):
             'training_losses': self.training_losses,
             'validation_losses': self.validation_losses
         }
-        state_path = '/cmlscratch','dtinubu','clone','Center-loss', 'logs', 'models' , file_name
-        torch.save(state_path)
+         state_path = os.path.join(model_dir, file_name)
+        torch.save(state, state_path)
