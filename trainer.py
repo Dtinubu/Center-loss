72d058c2a3af3859a14d3c792eefe95fd5b0a205
Dtinubu <73240396+Dtinubu@users.noreply.github.com>
2021-01-20 14:11:51 -0500

    Update trainer.py

--- trainer.py
+++ trainer.py
@@ -14,7 +14,7 @@ class Trainer(object):
             validation_dataloader, log_dir=False, max_epoch=100, resume=False,
             persist_stride=5, lamda=0.03, alpha=0.5):
 
-        self.log_dir =  dataset_dir
+        self.log_dir =  os.path.join('cmlscratch','dtinubu' ,'datasets', 'LFW')
         self.optimizer = optimizer
         self.model = model
         self.max_epoch = max_epoch
